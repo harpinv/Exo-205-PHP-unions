3ba1a73f01be5d386e8047af5f58d00ccf5bf7d3
vince <vincent.harpin@orange.fr>
2023-02-21 12:33:12 +0100

    exercice 205

--- index.php
+++ index.php
@@ -7,3 +7,51 @@
  * Utilisez UNION ALL pour afficher toutes les données y compris les doublons, affichez le résultat  à l'aide d'une boucle ou d'un print_r.
  * PS: Si vous utilisez un print_r, alors utilisez la balise <pre> pour un résultat plus propre.
  */
+
+$server = 'localhost';
+$user = 'root';
+$pwd = '';
+$db = 'mes_utilisateurs';
+
+try {
+    $connect = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pwd);
+
+    $user1 = $connect->prepare("
+            SELECT username FROM user
+            UNION
+            SELECT username FROM admin
+            UNION
+            SELECT username FROM client
+    ");
+
+    $liste = $user1->execute();
+
+    if($liste) {
+        foreach ($user1->fetchAll() as $value) {
+            echo "<pre>";
+            print_r($value);
+            echo "</pre>";
+        }
+    }
+
+    $user1 = $connect->prepare("
+            SELECT username FROM user
+            UNION ALL
+            SELECT username FROM admin
+            UNION ALL
+            SELECT username FROM client
+    ");
+
+    $liste = $user1->execute();
+
+    if($liste) {
+        foreach ($user1->fetchAll() as $value) {
+            echo "<pre>";
+            print_r($value);
+            echo "</pre>";
+        }
+    }
+}
+catch (PDOException $exception) {
+    echo "Erreur de connexion: " . $exception->getMessage();
+}
